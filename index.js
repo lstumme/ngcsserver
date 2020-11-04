9414f5db25e084549e7dc99d1e9b4468782d58d5
Ludovic Stumme <ludovic.stumme@gmail.com>
2020-11-04 15:43:33 +0100

    Lauching test

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public', 'index.html')));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
